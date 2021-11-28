bee3d02b39ec2ebd9c8a056ed05b0bd56263be96
flow-state-15 <purcellgroup88@gmail.com>
2021-11-27 18:18:50 -0600

    homepage and footer finished

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,8 +1,8 @@
 import "./HomePage.css";
 import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { Link, Redirect, useHistory } from "react-router-dom";
-import { login, logout } from "../../store/session";
+import { useHistory } from "react-router-dom";
+import { login, logout, restoreUser } from "../../store/session";
 
 export default function HomePage() {
   const history = useHistory();
@@ -12,7 +12,7 @@ export default function HomePage() {
 
   const dispatchDemoLogin = () => {
     dispatch(login("DougD@demo.dome", "DemoDome"));
-    history.push('/1')
+    history.push("/1");
     setDemo(true);
   };
 
@@ -26,14 +26,34 @@ export default function HomePage() {
 
   const onLogout = async (e) => {
     await dispatch(logout());
-    history.push("/")
+    history.push("/");
+  };
+
+  const enterApp = async () => {
+    if (user) {
+      await dispatch(restoreUser());
+      history.push("/@profile/:userId");
+    } else {
+      history.push("/login");
+    }
   };
 
   return (
-    <div>
+    <div className="home-wrapper">
       <div className="banner-img-container">
-          <img src="https://res.cloudinary.com/dan-purcell-2021/image/upload/v1637104657/robinhood_clone/home-background-1_p9cicp.webp" alt="banner-1" />
+        <img
+          src="https://res.cloudinary.com/dan-purcell-2021/image/upload/v1637104657/robinhood_clone/home-background-1_p9cicp.webp"
+          alt="banner-1"
+        />
+      <div className="home-text-overlay">
+        <p>
+          Trade in a simulated environment and view options data that is withheld from some popular brokers!<br/><br/> AlphaTrade can be used alongside your favorite chart analysis tools to get a feel for the market without signing up for a broker.
+        </p>
       </div>
+      </div>
+      <button className="nav-bar-buttons home-buttons" onClick={enterApp}>
+        Enter AlphaTrade
+      </button>
     </div>
   );
 }
